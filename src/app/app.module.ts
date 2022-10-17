490c576e2e0ffd15e33495091130d13ade48b79b
Cyril Casaucau <casaucau.cyril@protonmail.com>
2022-10-17 12:29:19 +0200

    Service Appel API NFTPort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {MarketplaceModule} from "./marketplace/marketplace.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {MarketplaceModule} from "./marketplace/marketplace.module";
   ],
   imports: [
     BrowserModule,
-    MarketplaceModule
+    MarketplaceModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/marketplace/list-nft/list-nft.component.ts
+++ src/app/marketplace/list-nft/list-nft.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Nft} from "../model/nfts.model";
 import {NftService} from "../nft.service";
+import {filter, map, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-list-nft',
@@ -11,11 +12,34 @@ export class ListNftComponent implements OnInit {
 
   public nfts: Array<Nft> = [];
 
+  public isLoading : boolean = false;
+
   constructor(private nftService: NftService) {
   }
 
   ngOnInit(): void {
-    this.nfts = this.nftService.getAll()
+    this.fetchData();
   }
 
+  private fetchData(): void {
+    // Sans bonus
+    // this.nftService.getAll().pipe(
+    //   map(res => res.body),
+    // ).subscribe(
+    //   nfts => {
+    //     this.nfts = nfts!.nfts;
+    //   });
+
+    // Bonus 1 et 2
+    this.isLoading = true;
+    this.nftService.getAll().pipe(
+      map(res => res.body),
+      switchMap(body => body!.nfts),
+      filter(nft => nft.metadata!.hasOwnProperty('edition'))
+    ).subscribe(
+      nft => {
+        this.isLoading = false;
+        this.nfts.push(nft);
+      });
+  }
 }
