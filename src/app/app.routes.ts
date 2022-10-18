a55f0e082a017de6aed0c618adda634bff44d5d8
Cyril Casaucau <casaucau.cyril@protonmail.com>
2022-10-18 09:54:22 +0200

    Mise en place d'une guards + fix exception quand objet vide dans le template de description

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import {Routes} from "@angular/router";
 import {ListNftComponent} from "./marketplace/list-nft/list-nft.component";
 import {DescriptionNftComponent} from "./marketplace/description-nft/description-nft.component";
+import {LogGuard} from "./marketplace/log.guard";
 
-export const ROUTES : Routes = [
+export const ROUTES: Routes = [
   {path: '', component: ListNftComponent},
-  {path: 'description/:blockchain/:contractid/:token', component: DescriptionNftComponent},
+  {path: 'description/:blockchain/:contractid/:token', component: DescriptionNftComponent, canActivate: [LogGuard]},
 ]
--- src/app/marketplace/description-nft/description-nft.component.ts
+++ src/app/marketplace/description-nft/description-nft.component.ts
@@ -27,5 +27,4 @@ export class DescriptionNftComponent implements OnInit {
       }
     });
   }
-
 }
